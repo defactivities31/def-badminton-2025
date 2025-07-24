fdd56a2a6998b9377a6cf98057a2a8f57d0ad660
defactivities31 <defactivities31@gmail.com>
2025-07-24 22:32:54 +0530

    Delete DEF-BADMINTON-2025 directory

--- DEF-BADMINTON-2025/js/bar.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const toggleButton = document.querySelector('.toggle-button');
-  const nav = document.querySelector('nav');
-
-  toggleButton.addEventListener('click', () => {
-    nav.classList.toggle('show');
-  });
-});
